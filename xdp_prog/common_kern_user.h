8bbc16357292e5e88ab1e8f6be10de3e089632ce
DTuan2016 <dtuan1608git@gmail.com>
2025-09-24 15:14:15 +0700

    Update

--- xdp_prog/common_kern_user.h
+++ xdp_prog/common_kern_user.h
@@ -9,13 +9,13 @@
 /*Config numbers of total data_points to training*/
 #define TRAINING_SET         3200
 /*Config numbers of flow to save to map xdp_flow_tracking or flow_dropped*/
-#define MAX_FLOW_SAVED       500
+#define MAX_FLOW_SAVED       300
 /*Config random forest*/
 /*MAX 60 trees with max_node = 256*/
 #define MAX_TREES            30
 #define MAX_NODE_PER_TREE    256
 #define MAX_SAMPLES_PER_NODE 256
-#define MIN_SPLIT_SAMPLES    4
+#define MIN_SPLIT_SAMPLES    2
 /*log2(TRAINING SET)*/     
 #define MAX_TREE_DEPTH       8
 /*Don't configure here*/
