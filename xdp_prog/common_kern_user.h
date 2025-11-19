4495a3113a1f2303fcf77be1b3243343f1b417cf
DTuan2016 <dtuan1608git@gmail.com>
2025-11-19 14:15:43 +0700

    Update throughput with type of total_bytes is __u64

--- xdp_prog/common_kern_user.h
+++ xdp_prog/common_kern_user.h
@@ -26,8 +26,8 @@ typedef __u64               fixed;
 typedef struct {
     __u64 time_in;
     __u64 proc_time;  /*proc_time += time_out - time_in*/
-    __u32 total_pkts;
-    __u32 total_bytes;
+    __u64 total_pkts;
+    __u64 total_bytes;
 } accounting;
 
 /* Flow key structure */
